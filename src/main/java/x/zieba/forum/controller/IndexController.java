22d9e438d7b0eddcc92400cee9590ec17adafb2d
zieba <dawid.zieba92@gmail.com>
2014-06-28 10:09:04 +0200

    adding hibernate

--- src/main/java/x/zieba/forum/controller/IndexController.java
+++ src/main/java/x/zieba/forum/controller/IndexController.java
@@ -1,4 +1,4 @@
-package x.zieba.blog.controller;
+package x.zieba.forum.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
